9d8db2c25c1d53892f630a806ebead151949ff8c
Justin Chan <justinchanuk@gmail.com>
2018-11-26 15:13:02 +0800

    Added Policyholder class with minimum init, print_details and run_transaction methods.

--- ips.py
+++ ips.py
@@ -1,6 +1,27 @@
 #import libraries - csv
 
 
+#Policyholder class
+class Policyholder:
+    
+    def __init__(self):
+        #create policyholder data
+        self.id="P0001" #Pxxxx
+        self.gender="M" #M, F
+        self.dob="19990101" #YYYYMMDD
+        self.smoker="Y" #Y, N
+        self.uw_status="standard" # standard, substandard
+        self.fab="Y" #Y, N - Fab is a healthy lifestyle programme
+
+    
+    def print_details(self):
+        details = (self.id, self.gender, self.dob, self.smoker, self.uw_status, self.fab)
+        print(("; ").join(details))
+
+    def run_transactions(self):
+        print("Create policy and claim transactions")
+
+
 #portfolio creation
 def run_sim(n):
     print("run sim")
@@ -8,6 +29,9 @@ def run_sim(n):
     for x in range(n):
         print("Running Policyholder: " + str(x))
         #Create policyholder and transaction
+        ph=Policyholder()
+        ph.print_details()
+        ph.run_transactions()
         output()
 
 
@@ -32,8 +56,9 @@ def housekeep():
 #declare variables
 num_ph = 10
 ph_filename = "policyholders.csv"
-claims_filename="claims.csv"
-policies_filename="policies.csv"
+clm_filename="claims.csv"
+pol_filename="policies.csv"
+chn_filename="channels_csv"
 print("Completed initiating variables")
 
 #run simulator
