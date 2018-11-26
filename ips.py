27b9e5c073458c7713e47835e87d85cc5e323444
Justin Chan <justinchanuk@gmail.com>
2018-11-26 14:45:03 +0800

    Initial commit. Outline code for insurance portfolio simulator

---
+++ ips.py
@@ -0,0 +1,29 @@
+#import libraries - csv
+
+
+#portfolio creation
+def run_sim(n):
+    print("run sim")
+    #create n number of policyholders, each with policies and claims
+    #for x in num:
+
+#input stuff
+def init():
+    print("Run init")
+
+#output stuff
+def output():
+    print("Run output")
+
+
+#declare variables
+num_ph = 1000
+ph_filename = "policyholders.csv"
+claims_filename="claims.csv"
+policies_filename="policies.csv"
+print("Completed initiating variables")
+
+#run simulator
+init()
+run_sim(num_ph)
+output()
