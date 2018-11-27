3ed588ca0524dfaaf7690877b775304ea6687262
Justin Chan <justinchanuk@gmail.com>
2018-11-27 11:02:46 +0800

    Added Claim class. Modelling such that Policy has Claims

--- ips.py
+++ ips.py
@@ -15,23 +15,49 @@ all_ch=[]
  
 #Policy class
 class Policy:
-    def __init__(self, id, policy_start, policy_end, product_id, channel_id, status):
+    def __init__(self, id, policy_start, policy_end, product_id, channel_id, sum_assured, claims, status):
         #create policy with id, start date, end date, status
         self.id=id #unique id of policy sold. Eg PL001
         self.policy_start=policy_start
         self.policy_end=policy_end
         self.product_id=product_id #product ID
         self.channel_id=channel_id #channel ID
+        self.sum_assured=sum_assured
+        self.claims=claims #list of claims
         self.status=status
+        
         
     def print_header(file_handle):
         file_handle.write("Policy_ID, Policy_Start, Policy_End, Product_ID, Channel_ID, Status\n")
 
-    def output_details(self, file_handle):
-        details = (self.id, self.policy_start, self.policy_end, self.product_id, self.channel_id, self.status,)
+    def output_details(self, pol_file_handle, clm_file_handle):
+        #output to policy.csv
+        details = (self.id, self.policy_start, self.policy_end, self.product_id, self.channel_id, self.status)
         pol_line=(", ").join(details)
         print(pol_line)
-        file_handle.writelines(pol_line)
+        pol_file_handle.writelines(pol_line)
+        pol_file_handle.write("\n")
+        #output claims!
+        for cl in self.claims:
+            cl.output_details(clm_file_handle)
+
+
+#Claim class
+class Claim:
+    def __init__(self, id, policy_id, claim_amount, claim_reason):
+        self.id=id
+        self.policy_id=policy_id
+        self.claim_amount = claim_amount #should be based on sum assured
+        self.claim_reason = claim_reason #should be based product risk/conditions covered
+    
+    def print_header(file_handle):
+        file_handle.write("Claim_ID, Policy_ID, Claim_Amount, Claim_Reason\n")
+
+    def output_details(self, file_handle):
+        details = (self.id, self.policy_id, str(self.claim_amount), self.claim_reason)
+        cl_line=(", ").join(details)
+        print(cl_line)
+        file_handle.writelines(cl_line)
         file_handle.write("\n")
 
 #Product class
@@ -45,7 +71,7 @@ class Product:
         file_handle.write("Product_ID, Product_Name\n")
 
     #setup products - class function
-    def setup_products():
+    def setup_products(file_handle):
         #create multiple products default templates
         p1=Product("PD001", "Term Life")
         p2=Product("PD002", "Whole of Life")
@@ -54,8 +80,8 @@ class Product:
         all_prod=[p1, p2, p3, p4]
         
         for p in all_prod:
-            file_handles[4].write(",".join([str(p.id), str(p.name)]))
-            file_handles[4].write("\n")
+            file_handle.write(",".join([str(p.id), str(p.name)]))
+            file_handle.write("\n")
 
 #Channel class
 class Channel:
@@ -69,7 +95,7 @@ class Channel:
         file_handle.write("Channel_ID, Channel_Type, Channel_Name\n")
 
     #setup channels - class function
-    def setup_channels():
+    def setup_channels(file_handle):
         #create multiple products default templates
         c1=Channel("CH0001", "Bank", "ABC Bank, Branch 1")
         c2=Channel("CH0002", "Bank", "ABC Bank, Branch 2")
@@ -92,8 +118,8 @@ class Channel:
             all_ch.append(Channel( "CH" + "0"*(4-i_length) + str(i) , "Agency" , "Agent " + str(i)))
         
         for c in all_ch:
-            file_handles[3].write(", ".join([str(c.id), c.type, c.name]))
-            file_handles[3].write("\n")
+            file_handle.write(", ".join([str(c.id), c.type, c.name]))
+            file_handle.write("\n")
 
 #Policyholder class
 class Policyholder:
@@ -126,7 +152,7 @@ class Policyholder:
         #for policy created, identify channel, product, then claims
         for x in range(num_policies):
             print("create policy with product, agent, policy start date, term")
-            pol = Policy("PL001", "20101010", "20151009", "PD001", "CH0011", "Lapse")
+            pol = Policy("PL001", "20101010", "20151009", "PD001", "CH0011", 100000, [], "")
             print("Housekeep - add to list of policies")
             ph_pols.append(pol)
 
@@ -135,12 +161,12 @@ class Policyholder:
         #then update policy files to reflect status
         ph_pols[0].status="Lapse"
         ph_pols[1].status="Claim"
+        ph_pols[1].claims.append(Claim("CL0001", ph_pols[1].id, ph_pols[1].sum_assured, "Death"))
         ph_pols[2].status="Mature"
 
         #output to policy.csv file
         for p in ph_pols:
-                p.output_details(file_handles[2])
-
+                p.output_details(file_handles[2], file_handles[1])
     
     def run_transactions(self):
         print("Create policy and claim transactions")
@@ -174,10 +200,13 @@ def init():
     #print headers
     Policyholder.print_header(file_handles[0])
     Policy.print_header(file_handles[2])
+    Claim.print_header(file_handles[1])
     Product.print_header(file_handles[4])
+    
     #setup products, channels etc.
-    Product.setup_products()
-    Channel.setup_channels()
+    Product.setup_products(file_handles[4])
+    Channel.setup_channels(file_handles[3])
+    
 
 #housekeeping stuff
 def housekeep():
