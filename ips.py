d7db7b6acd5869261a0c6580793f95cc3e40e257
Justin Chan <justinchanuk@gmail.com>
2018-11-27 09:17:31 +0800

    Updated Policy class. Initial Channel class. Updated output_details for various objects

--- ips.py
+++ ips.py
@@ -6,7 +6,7 @@ num_ph = 10
 ph_filename = "policyholders.csv"
 clm_filename="claims.csv"
 pol_filename="policies.csv"
-chn_filename="channels_csv"
+chn_filename="channels.csv"
 pd_filename="products.csv"
 all_files=[]
 file_handles=[]
@@ -14,17 +14,23 @@ all_prod=[]
 
 #Policy class
 class Policy:
-    def __init__(self, id):
+    def __init__(self, id, policy_start, policy_end, product_id, status):
         #create policy with id, start date, end date, status
         self.id=id #unique id of policy sold. Eg PL001
         self.policy_start="20101010"
         self.policy_end="20151009"
+        self.product_id="PD001" #product ID
         self.status="Lapse"
-        self.product="PD001" #product ID
-
+        
     def print_header(file_handle):
-        file_handle.write("ID, Policy_Start, Policy_End, Product_ID,Status\n")
+        file_handle.write("Policy_ID, Policy_Start, Policy_End, Product_ID,Status\n")
 
+    def output_details(self, file_handle):
+        details = (self.id, self.policy_start, self.policy_end, self.product_id, self.status,)
+        pol_line=(", ").join(details)
+        print(pol_line)
+        file_handle.writelines(pol_line)
+        file_handle.write("\n")
 
 #Product class
 class Product:
@@ -33,8 +39,19 @@ class Product:
         self.id=id #id of product. Eg. PD001
         self.name=name #name of product. Eg. Term Life
 
-    def print_header(file_handle):
-        file_handle.write("Product_ID, Name\n")
+    def print_header(file_handle): #no need 'self' argument. This is a class function
+        file_handle.write("Product_ID, Product_Name\n")
+
+#Channel class
+class Channel:
+
+    def __init__(self, id, name):
+        self.id=id #id of channel. Eg CH1 for bank, CH2 for IFA, CH3 for Agent
+        self.name=name #channel name. Bank, IFA, Agent
+
+    def print_header(file_handle): #no need 'self' argument. This is a class function
+        file_handle.write("Channel_ID, Channel_Name\n")
+    
 
 #Policyholder class
 class Policyholder:
@@ -53,20 +70,35 @@ class Policyholder:
         file_handle.write(",".join(["Policyholder_ID", "Gender", "DOB", "Smoker", "UW_Status", "Fab"]))
         file_handle.write("\n")
     
-    def output_details(self):
+    def output_details(self, file_handle):
         details = (self.id, self.gender, self.dob, self.smoker, self.uw_status, self.fab)
         ph_line=(", ").join(details)
         print(ph_line)
-        file_handles[0].writelines(ph_line)
-        file_handles[0].write("\n")
+        file_handle.writelines(ph_line)
+        file_handle.write("\n")
 
     def transact_sim(self):
         #purchase k number of policies. Currently only have 4 products.
-        num_policies = 3 
+        num_policies = 3
+        ph_pols=[]
         #for policy created, identify channel, product, then claims
-        for x in range(3):
+        for x in range(num_policies):
             print("create policy with product, agent, policy start date, term")
-            print("determine if claimed")
+            pol = Policy("PL001", "20101010", "20151009", "PD001", "Lapse")
+            print("Housekeep - add to list of policies")
+            ph_pols.append(pol)
+
+
+        #decide what happens to policyholder (ie. dies, claims, lapses)
+        #then update policy files to reflect status
+        ph_pols[0].status="Lapse"
+        ph_pols[1].status="Claim"
+        ph_pols[2].status="Mature"
+
+        #output to policy.csv file
+        for p in ph_pols:
+                p.output_details(file_handles[2])
+
     
     def run_transactions(self):
         print("Create policy and claim transactions")
@@ -82,8 +114,9 @@ def run_sim(n):
         print("Running Policyholder: " + str(x))
         #Create policyholder and transaction
         ph=Policyholder()
-        ph.output_details()
         ph.run_transactions()
+        #ouput details
+        ph.output_details(file_handles[0])
 
 
 #init files
